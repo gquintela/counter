1bfb22d30ae91fccc8580c78c2ea7b1c7aa23f77
gquintela <gquintela@dc.uba.ar>
2020-03-17 01:21:27 -0300

    created tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,39 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>there are no tags!</p>;
+    } else {
+      return (
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.renderTags()}
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2";
+    classes += this.state.count === 0 ? " badge-warning" : " badge-primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
