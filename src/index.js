12a3cb151fd6a8480fad6333159a6feef24a72d4
gquintela <gquintela@dc.uba.ar>
2020-03-16 21:54:52 -0300

    counter object

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
