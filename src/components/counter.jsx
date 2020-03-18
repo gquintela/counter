2fe594bef5dfe63df32613365e1922828d4fc5c4
gquintela <gquintela@dc.uba.ar>
2020-03-18 19:28:51 -0300

    created Counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"]
   };
 
@@ -12,16 +12,16 @@ class Counter extends Component {
     } else {
       return (
         <ul>
-          {this.state.tags.map(tag => (
+          {/* {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
-          ))}
+          ))} */}
         </ul>
       );
     }
   }
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -41,12 +41,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2";
-    classes += this.state.count === 0 ? " badge-warning" : " badge-primary";
+    classes += this.state.value === 0 ? " badge-warning" : " badge-primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
