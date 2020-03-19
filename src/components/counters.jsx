70f8b7938aa0116a9c33d8a1456e86fe3257addf
gquintela <gquintela@dc.uba.ar>
2020-03-19 19:10:59 -0300

    project finished

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,25 +3,26 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const {
+      onAddCounter,
+      onReset,
+      counters,
+      onDelete,
+      onIncrement
+    } = this.props;
     return (
       <div>
-        <button
-          onClick={this.props.onAddCounter}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onAddCounter} className="btn btn-primary btn-sm m-2">
           Add Counter
         </button>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           RESET
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
